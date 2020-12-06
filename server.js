faff67b532bf5afbe247c35b4bb34a180fc158b2
Pedal <jmb181@humboldt.edu>
2020-12-06 13:15:00 -0800

    created api endpoints for db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +7,86 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database - create db object
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the election database');
+});
+
+//GET all candidates
+app.get('/api/candidates', (req,res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err){
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+
+// GET a single candidate
+app.get('/api/candidate/:id', (req,res) => {
+    const sql = `SELECT * FROM candidates
+                WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql,params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+
+// CREATE a candidate 
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES (?,?,?,?)`;
+// const params = [1,'Ronald', 'Firbank',1];
+// db.run(sql, params, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// });
+
+// DELETE a candidate
+app.delete('/api/candidate/:id', (req,res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    
+    db.run(sql,params, function(err,result){
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+
 // Default response for any other request (not found) Catch all - must be after all gets?
 app.use((req,res) => {
     res.status(404).end();
 });
 
-// start the express.js server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// start the express.js server after DB connection
+db.on('open', () => {
+   app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`); 
+    })
 });
